be4d0dc2865185fb1a0020892ce3b5c04e04bf4f
kehinde-odunuga <omobolaji2@gmail.com>
2025-05-16 19:48:05 +0100

    demoJS

---
+++ demo.js
@@ -0,0 +1,6 @@
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width;
+    //console.log ("The Area of the Rectangle is " + area);
+    return area;
+}
+console.log(calculateAreaOfRectangle(10, 20));
